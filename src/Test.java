4592bb7892a41bb5ee591f52c65cf667eda545ae
wangshuai <shuai.ws@foxmail.com>
2019-06-03 16:45:05 +0800

    新加代码

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("你这个小坏蛋");
+        String s = "1234";
+        String m = "12345678";
+        System.out.println(s+m);
     }
 }
